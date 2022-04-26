e6cc3be97f5fafe5f9b4389159e2719c8db80ea8
wellington <marcelinoc.wellington@gmail.com>
2022-04-25 21:03:19 -0300

    alteração do projeto

--- consumoAPI/teste-set.js
+++
@@ -1,9 +0,0 @@
-const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5, 1995];
-
-function valoresUnicos(arr) {
-  const mySet = new Set(arr);
-
-  return [...mySet];
-}
-
-console.log(valoresUnicos(meuArray));
